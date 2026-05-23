f82f8acd7329da9f8729845498adff9d65642ebf
venkatesh <ovenkatramana01@gmail.com>
2026-05-23 12:48:40 +0530

    tools commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.tools.tools import web_search,scrape_url
+
+
+# web_search_result = web_search.invoke(
+#     {"query": "latest news on AI?"}
+# )
+# print(web_search_result)
+
+
+scrape_result = scrape_url.invoke(
+    {"url": "https://guides.library.georgetown.edu/ai/news"}
+)
+print(scrape_result)
